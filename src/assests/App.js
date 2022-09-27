b7c94fd34902b8dbd0117d4701106ce0742345d6
ShrutiGova <89659936+ShrutiGova@users.noreply.github.com>
2022-09-27 22:34:17 +0530

    Delete App.js

--- src/assests/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PermanentDrawer from "./drawer";
-export default function App() {
-  return (
-    <React.Fragment>
-      <PermanentDrawer />
-    </React.Fragment>
-  );
-}
